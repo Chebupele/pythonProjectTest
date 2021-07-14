2abf0b1cee7f4d9f08a72307222e60449e7bc5a8
Chebupele <taskmasterden@gmail.com>
2021-07-14 16:38:58 +0300

    lists

---
+++ listtest.py
@@ -0,0 +1,13 @@
+words = ['мыла', 'мама', 'раму', 'мама',]
+list_object = [1, 2, 'c', 'строка', [1, 2, 3], ['я', 'ю', 'у']]
+print(words[2])
+print(list_object[4][2])
+print(len(words))
+print(len(list_object))
+print(words.index('мама'))
+words.append("papa")
+words.insert(1, 'окно')
+print(words.count('мама'))
+a = 'окно'
+print(a.count('о'))
+print(a[1])
