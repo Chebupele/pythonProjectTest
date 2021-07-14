7152941ef7416d9283ab2fe5612a75c0cc7912f0
Chebupele <taskmasterden@gmail.com>
2021-07-14 15:21:37 +0300

    6

---
+++ zadacha5.py
@@ -0,0 +1,24 @@
+y = input('Введите номер месяца')
+if y == '1' :
+    print('Зима')
+elif y == '2' :
+    print('Зима')
+elif y == '3':
+    print('Весна')
+elif y == '4':
+    print('Весна')
+elif y == '5':
+    print('Весна')
+elif y == '6':
+    print('Лето')
+elif y == '7':
+    print('Лето')
+elif y == '8':
+    print('Лето')
+elif y == '9':
+    print('Осень')
+elif y == '10':
+    print('Осень')
+elif y == '11':
+    print('Осень')
+else: print('Зима')
